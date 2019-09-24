a40a005ff3c5a5a479a5ff2ebd648cb5c42273c6
syeong <tjsdud7379@gmail.com>
2019-09-24 14:05:42 +0900

    server init

---
+++ src/config/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    jwtSecret: 'long-live-the-ionic-academy',
+    db: 'mongodb://localhost/shary',
+
+    //네이버 open api 
+    client_id : '4BAZEL9fhMV7MnBiJx1T',
+    client_secret : 'BWYeR798st',
+
+    //네이버 지도 api
+    NAVER_CLIENT_ID     : 'yce4qlo07o',
+    NAVER_CLIENT_SECRET : 'NhCdEtu13Zfr2MB9UreEnhRhPEWjEvFKMk4s5ull',
+
+    kakao: {
+        clientID : '9e4ff89c213d34be116b6915b6e7c10b',
+        callbackURL: 'http://localhost:5000'
+    }
+};
