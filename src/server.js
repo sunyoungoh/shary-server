6fb3e777d2ba907fc458672dea0fb5df3dbfbb08
sgjh4040 <sgjh4040@naver.com>
2019-09-24 15:04:57 +0900

    dotenv 추가

--- src/server.js
+++ src/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const mongoose = require('mongoose');
-const config = require('./config/config');
-const dotenv = require('dotenv');
-dotenv.config();
+const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const morgan = require('morgan');
@@ -58,7 +56,7 @@ app.use('/api/search', openApiRouter);
 app.use('/api/auth', kakaoRouter);
 app.use('/api', userRouter);
 
-mongoose.connect(config.db, {
+mongoose.connect(process.env.db, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
